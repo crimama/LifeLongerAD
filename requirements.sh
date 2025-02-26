d42aa0379d9e201825a7b96456b35b33cd8a534f
crimama <crimama2@gmail.com>
2025-02-26 06:54:12 +0000

    [2025-02-26] add

--- requirements.sh
+++ requirements.sh
@@ -21,6 +21,12 @@ pip install faiss-gpu
 pip install av==10.0.0
 pip install adamp
 pip install albumentations==1.3.1
+pip install river 
+pip install jsonargparse
+pip install lightning
+pip install openvino
+pip install 'git+https://github.com/katsura-jp/pytorch-cosine-annealing-with-warmup'
+
 
 apt-get update && apt-get install libgl1
 pip install opencv-python==4.3.0.38
@@ -29,6 +35,7 @@ apt-get install libglib2.0-0
 
 
 # pip install torch==1.10.1+cu111 torchvision==0.11.2+cu111 torchaudio==0.10.1 -f https://download.pytorch.org/whl/cu111/torch_stable.html
+# pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu121
 # pip install tqdm tensorboard setuptools==58.0.4 opencv-python scikit-image scikit-learn matplotlib seaborn ftfy regex numpy==1.26.4
 
 # pip install 'git+https://github.com/katsura-jp/pytorch-cosine-annealing-with-warmup'
