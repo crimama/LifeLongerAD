769d7c5fc546c6b4fe9694a87ce724232a66508c
crimama <crimama2@gmail.com>
2025-04-14 12:30:07 +0000

    experiment config

--- run.sh
+++ run.sh
@@ -3,27 +3,37 @@
 # VISA  'candle capsules cashew chewinggum fryum macaroni1 macaroni2 pcb1 pcb2 pcb3 pcb4 pipe_fryum'
 # MVTecLoco 'breakfast_box  juice_bottle  pushpins  screw_bag  splicing_connectors'
 # MPDD 'tubes metal_plate connector bracket_white bracket_brown bracket_black'
-gpu_id=$1
+exp_id=$1
 
 # GPU ID에 따라 method_setting 설정
-if [ "$gpu_id" -eq 0 ]; then
+if [ "$exp_id" -eq 141 ]; then
+    method_setting="cfgcad"
+    continual='true'
+    dataset='mvtecad_141'
+    exp='14_1_with_1_step'
+    gpu_id=0
+elif [ "$exp_id" -eq 35 ]; then
     method_setting="cfgcad"
     continual='true'
     gpu_id=0
-elif [ "$gpu_id" -eq 1 ]; then
-    method_setting="proxycore"
+    exp='3_5_with_5_step'
+    dataset='mvtecad_141'
+elif [ "$exp_id" -eq 1015 ]; then
+    method_setting="cfgcad"
     continual='false'
-    gpu_id=1
-elif [ "$gpu_id" -eq 2 ]; then
-    method_setting="proxycore"
+    exp='10_1_with_5_step'
+    gpu_id=0
+elif [ "$exp_id" -eq 1051 ]; then
+    method_setting="cfgcad"
     continual='false'
-    gpu_id=2
+    exp='10_5_with_1_step'
+    gpu_id=0
 else
-    echo "Invalid GPU ID. Please use 0 or 1."
+    echo "Invalid EXP number"
     exit 1
 fi
 
-dataset='mvtecad'
+
 
 
 for c in $continual
@@ -48,7 +58,7 @@ do
                     CUDA_VISIBLE_DEVICES=$gpu_id python main.py \
                         default_setting=./configs/default/$d.yaml \
                         model_setting=./configs/model/$m.yaml \
-                        DEFAULT.exp_name=baseline-3_5_with_5_step \
+                        DEFAULT.exp_name=baseline4_2-$exp \
                         CONTINUAL.continual=$c \
                         CONTINUAL.online=$o \
                         CONTINUAL.method.name=$cm
@@ -57,8 +67,3 @@ do
         done
     done
 done
-
-# tmux 0 | gpu : 6 | Nsoftmax-focal_loss
-# tmux 1 | gpu : 7 | Nsoftmax_crossentropy
-# tmux 2 | gpu : 0 | ArcMarginProduct
-# tmux 3 | gpu : 1 | Softtriple
